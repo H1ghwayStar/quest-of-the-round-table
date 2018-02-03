5993aa715b550616c3354503b6c027d024cc0bfc
MuhammmadNAhmad <naseernahmad@cmail.carleton.ca>
2018-02-03 18:56:46 -0500

    update .exe files

--- PROJ3004/PROJ3004/Gameboard.cs
+++ PROJ3004/PROJ3004/Gameboard.cs
@@ -6,51 +6,62 @@ namespace PROJ3004
     {
         public static void Main(string[] args)
         {
+            
+            int NumberOfPlayers;
+            string input;
+
             //Test Cases
-
             Foe Dragon = new Foe("Dragon", 50);
-            Console.WriteLine(Dragon);
+            //Console.WriteLine(Dragon);
 
-            Foe Boar = new Foe("Boar", 5);             Console.WriteLine(Boar);
+            Foe Boar = new Foe("Boar", 5);             //Console.WriteLine(Boar);
 
             Weapon Excalibur = new Weapon("Excalibur", 30);
-            Console.WriteLine(Excalibur);
+            //Console.WriteLine(Excalibur);
 
             Weapon Lance = new Weapon("Lance", 20);
-            Console.WriteLine(Lance);
+            //Console.WriteLine(Lance);
 
             Ally KingArthur = new Ally("King Arthur", 10, 2);
-            Console.WriteLine(KingArthur);
+            //Console.WriteLine(KingArthur);
 
             Amour amour = new Amour();
-            Console.WriteLine(amour);
+            //Console.WriteLine(amour);
 
             Ally KingPellinore = new Ally("King Pellinore", 10);
-            Console.WriteLine(KingPellinore);
+            //Console.WriteLine(KingPellinore);
 
             Test TestOfTheQuestingBeast = new Test("Test of the Questing Beast", 4);
-            Console.WriteLine(TestOfTheQuestingBeast);
+            //Console.WriteLine(TestOfTheQuestingBeast);
 
             Test TestOfTemptation = new Test("Test of Temptation", 0);
-            Console.WriteLine(TestOfTemptation);
+            //Console.WriteLine(TestOfTemptation);
 
             Tournament AtOrkey = new Tournament("Orkey", 2);
-            Console.WriteLine(AtOrkey);
+            //Console.WriteLine(AtOrkey);
 
             Tournament AtYork = new Tournament("York", 0);
-            Console.WriteLine(AtYork);
+            //Console.WriteLine(AtYork);
 
             Event QueensFavour = new Event("Queens Favour", 0);
-            Console.WriteLine(QueensFavour);
+            //Console.WriteLine(QueensFavour);
 
             Event CourtCalledToCamelot = new Event("Court Called To Camelot", 0, 0, true, 0, 0);
-            Console.WriteLine(CourtCalledToCamelot);
+            //Console.WriteLine(CourtCalledToCamelot);
 
             Quest SearchForTheQuestingBeast = new Quest("Search For The Questing Beast", 4, "none");
-            Console.WriteLine(SearchForTheQuestingBeast);
+            //Console.WriteLine(SearchForTheQuestingBeast);
 
             Quest SlayTheDragon = new Quest("Slay The Dragon", 3, "Dragon");
-            Console.WriteLine(SlayTheDragon);
+            //Console.WriteLine(SlayTheDragon);
+
+            Console.WriteLine("<---------Knight of the Round Table--------->\n\n");
+            Console.WriteLine("How Many player will be playing today?");
+            input = Console.ReadLine();
+            NumberOfPlayers = Int32.Parse(input);
+            Console.WriteLine(NumberOfPlayers);
+
+
         }
     }
 }
