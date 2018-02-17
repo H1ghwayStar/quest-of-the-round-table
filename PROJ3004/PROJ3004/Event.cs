c290ba38e798c29854fb456a734de0b192253c5b
MuhammmadNAhmad <naseernahmad@cmail.carleton.ca>
2018-02-16 21:16:54 -0500

    Implemented Events

--- PROJ3004/PROJ3004/Event.cs
+++ PROJ3004/PROJ3004/Event.cs
@@ -1,49 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PROJ3004
 {
-	public class Event : Story
-	{
-		protected int shieldModifier;
-		private int adventureCardModifier;
-		private bool eliminateAllies;
-		private int weaponCardModifier;
-		private int foeCardModifier;
+    public class Event : Story
+    {
+        private int shieldModifier;
+        private int adventureCardModifier;
+        private bool eliminateAllies;
+        private int weaponCardModifier;
+        private int foeCardModifier;
+        private string description;
+        private string whosAffected;
+        public static readonly List<string> playersTargeted = new List<String> { "LowestRankAndShield", "All", "AllExceptDrawer", "DrawerOnly", "HighestRanked", "LowestRanked", "Next" };
 
-		public Event(string name, int shieldModifier, int adventureCardModifier, bool eliminiateAllies, int weaponCardModifier, int foeCardModifier)
-		{
-			this.name = name;
-			this.shieldModifier = shieldModifier;
-			this.adventureCardModifier = adventureCardModifier;
-			this.eliminateAllies = eliminiateAllies;
-			this.weaponCardModifier = weaponCardModifier;
-			this.foeCardModifier = foeCardModifier;
-		}
+        public Event(string name, int shieldModifier, int adventureCardModifier, bool eliminiateAllies, int weaponCardModifier,
+            int foeCardModifier, string whosAffected, string description)
+        {
+            this.name = name;
+            this.shieldModifier = shieldModifier;
+            this.adventureCardModifier = adventureCardModifier;
+            this.eliminateAllies = eliminiateAllies;
+            this.weaponCardModifier = weaponCardModifier;
+            this.foeCardModifier = foeCardModifier;
+            this.description = description;
+            this.whosAffected = whosAffected;
+        }
 
-		public int GetAdventureCardModifier(){
-			return adventureCardModifier;
-		}
+        public int GetAdventureCardModifier()
+        {
+            return adventureCardModifier;
+        }
 
-		public bool GetEliminateAllies(){
-			return eliminateAllies;
-		}
+        public bool GetEliminateAllies()
+        {
+            return eliminateAllies;
+        }
 
-		public int GetWeaponCardModifier(){
-			return weaponCardModifier;
-		}
+        public int GetWeaponCardModifier()
+        {
+            return weaponCardModifier;
+        }
 
-		public int GetFoeCardModifier(){
-			return foeCardModifier;
-		}
+        public int GetFoeCardModifier()
+        {
+            return foeCardModifier;
+        }
 
-		public int GetShieldModifier(){
-			return shieldModifier;
-		}
+        public int GetShieldModifier()
+        {
+            return shieldModifier;
+        }
 
-		public override string ToString ()
-		{
-            return string.Format("Name: " + name + ", Shield Modified: " + shieldModifier + ", Adventure Cards Modified: " + adventureCardModifier + ", Allies eliminated? " + eliminateAllies + ", Wepon Cards Modified: " + weaponCardModifier + ", Foe Cards Modified: " + foeCardModifier);
-		}
-	}
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public string GetWhosAffected()
+        {
+            return whosAffected;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name: " + name + ", Shield Modifier: " + shieldModifier + ", Adventure Cards Modifier: " + adventureCardModifier
+                + ", Allies eliminated? " + eliminateAllies + ", Weapon Cards Modifier: " + weaponCardModifier + ", Foe Cards Modifier: " + foeCardModifier
+                + "who's affected?: " + whosAffected + "\n" + description);
+        }
+    }
 }
 
--- PROJ3004/PROJ3004/Player.cs
+++ PROJ3004/PROJ3004/Player.cs
@@ -4,51 +4,54 @@ using System.Collections.Generic;
 
 namespace PROJ3004
 {
-	public class Player
-	{	//Rethink access modifiers for all this stuff later
-		private string name;
-		public int shields;
-		public string rank;
-		public const int MAX_CARDS_HAND = 12;
-		public List <Ally> alliesInPlay = new List <Ally> ();
-		public List <Adventure> cardsInHand = new List <Adventure>(); //reconsider visibility of this field
-		public List<Adventure> discardPile = new List<Adventure>(); //reconsider visibility of this field
-		public static readonly Dictionary<string, int> rankDictionary = new Dictionary<string, int>(){ //rank and associated battle points
-			{"Squire",5},
-			{"Knight",10},
-			{"Champion Knight", 20},
-			{"Knight of the Round", 9001}
-		};
+    public class Player
+    {   //Rethink access modifiers for all this stuff later
+        private string name;
+        public int shields;
+        public string rank;
+        public static readonly int MAX_CARDS_HAND = 12;
+        public List<Ally> alliesInPlay = new List<Ally>();
+        public List<Adventure> cardsInHand = new List<Adventure>(); //reconsider visibility of this field
+        public List<Adventure> discardPile = new List<Adventure>(); //reconsider visibility of this field
+        public static readonly Dictionary<string, int> rankDictionary = new Dictionary<string, int>(){ //rank and associated battle points
+            {"Squire",5},
+            {"Knight",10},
+            {"Champion Knight", 20},
+            {"Knight of the Round", 9001}
+        };
 
-		private static int numPlayers = 1;
-		//private int playerNum;
+        private static int numPlayers = 1;
+        //private int playerNum;
 
-		public Player (string name, string rank = "Squire", int shields = 0)
-		{
-			this.name = name;
-			this.rank = rank;
-			this.shields = shields;
+        public Player(string name, string rank = "Squire", int shields = 0)
+        {
+            this.name = name;
+            this.rank = rank;
+            this.shields = shields;
 
-			//this.playerNum = numPlayers;
-			numPlayers++;
-		}
+            //this.playerNum = numPlayers;
+            numPlayers++;
+        }
 
-		public string getName(){
-			return name;
-		}
-			
-		//discard method here
-		public void discard(int discardedCards){
-			if (cardsInHand.Count == 0)
-				Console.WriteLine ("Can't discard from an empty hand!!!");
+        public string getName()
+        {
+            return name;
+        }
 
-			for(int i = 0; i < discardedCards; i++){
-				discardPile.Add(cardsInHand[cardsInHand.Count-1]);
-				cardsInHand.RemoveAt (cardsInHand.Count - 1);
-			}
-		}
-		//rank-up method here
+        //discard method here
+        public void discard(int discardedCards)
+        {
+            if (cardsInHand.Count == 0)
+                Console.WriteLine("Can't discard from an empty hand!!!");
 
-	}
+            for (int i = 0; i < discardedCards; i++)
+            {
+                discardPile.Add(cardsInHand[cardsInHand.Count - 1]);
+                cardsInHand.RemoveAt(cardsInHand.Count - 1);
+            }
+        }
+        //rank-up method here
+
+    }
 }
 
